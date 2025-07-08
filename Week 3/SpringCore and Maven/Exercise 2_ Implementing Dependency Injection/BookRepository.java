ec97ad75cc9f065eeb865585b4d3a2d1277e6982
iswaryaayyanar <iswaryaayyanar95@gmail.com>
2025-07-08 15:26:28 +0530

    Add files via upload

---
+++ Week 3/SpringCore and Maven/Exercise 2_ Implementing Dependency Injection/BookRepository.java
@@ -0,0 +1,6 @@
+package com.library.repository;
+public class BookRepository{
+    public String getBookDetails(){
+        return "Title: To Kill a Mockingbird, Author: Harper Lee, ISBN: 12345";
+    }
+}
